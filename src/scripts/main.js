73a000f9d81f0a8f6d42df80774bb45aaf40af2a
Alexey Kravets <kravecoleksiy@gmail.com>
2025-05-02 20:50:36 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write code here
+const centerSpider = () => {
+  const spiderElement = document.querySelector('img');
+
+  spiderElement.style.top = '50%';
+  spiderElement.style.left = '50%';
+  spiderElement.style.transform = 'translate(-50%, -50%)';
+};
+
+centerSpider();
