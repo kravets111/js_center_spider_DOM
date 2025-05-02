f50307c9c43aaf0dac6bd3d9a4148ad6c0206b50
Alexey Kravets <kravecoleksiy@gmail.com>
2025-05-02 21:07:39 +0300

    make build

---
+++ js_center_spider_DOM.49dc87b3.js
@@ -0,0 +1,2 @@
+"use strict";var centerSpider=function(){var e=document.querySelector("img");e.style.position="absolute",e.style.top="50%",e.style.left="50%",e.style.transform="translate(-50%, -50%)"};centerSpider();
+//# sourceMappingURL=js_center_spider_DOM.49dc87b3.js.map
