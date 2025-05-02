26d93d34908ab7021c6510c956a787472fb4a82b
Alexey Kravets <kravecoleksiy@gmail.com>
2025-05-02 21:06:54 +0300

    add position

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,7 @@
 const centerSpider = () => {
   const spiderElement = document.querySelector('img');
 
+  spiderElement.style.position = 'absolute';
   spiderElement.style.top = '50%';
   spiderElement.style.left = '50%';
   spiderElement.style.transform = 'translate(-50%, -50%)';
